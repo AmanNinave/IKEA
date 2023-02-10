4742db1d2ea1fd59e8b4c77fb65b0612da881b52
AmanNinave <amanninave987@gmail.com>
2023-02-10 13:30:11 +0530

    cart setUp

--- ikea-clone/src/Pages/ProductList/Sofa/Sofa1.jsx
+++ ikea-clone/src/Pages/ProductList/Sofa/Sofa1.jsx
@@ -3,10 +3,9 @@ import './Sofa1.css'
 import Sofadata from './../../../JsonFiles/sofa1.json'
 import heart from './../../../Logo/heart.png'
 import cart from './../../../Logo/shopping-cart.png'
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
-</style>
-
+import cartAction from "../../../Redux/Action/cartAction";
+import { useDispatch , useSelector  } from 'react-redux'
+import wishlistAction from "../../../Redux/Action/wishlistAction";
 
 function Sofa1() {
 
@@ -14,10 +13,24 @@ function Sofa1() {
 
     const dta = Sofadata.moreProducts.productWindow;
 
+    const dispatch = useDispatch();
+
+    const storeData = useSelector((store) => {
+            return store
+    })
+
+    function addToCart ( elem ) {
+        cartAction(elem , dispatch);
+    } 
+
+    function addToWishlist ( elem ) {
+        wishlistAction(elem , dispatch );
+    }
+
     return (
         <div  >
 
-            <div className="Filters_productList" style={{ height: "120px" , border : "1px solid black" }}>
+            <div className="Filters_productList" style={{  }}>
 
                 <button>Sort</button>
                 <button>Number of Seats</button>
@@ -38,8 +51,12 @@ function Sofa1() {
                                     <h3>{elem.name}</h3>
                                     <p>{elem.typeName}</p>
                                     <h2><span style={{fontSize : "15px"  }}>{elem.salesPrice.current.prefix}</span>{elem.salesPrice.current.wholeNumber}</h2>
-                                    <img src={cart} alt="" width="30px" style={{ marginRight : "10px"  }}/>
-                                    <img src={heart} alt="" width="25px" />
+                                    <img src={cart} alt="" width="30px" style={{ marginRight : "10px"  }}   onClick={() => {
+                                        addToCart(elem);
+                                    }}/>
+                                    <img src={heart} alt="" width="25px" onClick={() => {
+                                        addToWishlist(elem) ;
+                                    }}/>
                                     
 
                                 </div>
